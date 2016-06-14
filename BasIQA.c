0c57ffaf445af677f95fe0c778592b9e19ee9a62
Bruno Andrade <Bruno_andrade_9898@hotmail.com>
2016-06-14 20:18:50 -0300

    Adicionando comentarios no codigo fonte

--- BasIQA.c
+++ BasIQA.c
@@ -1,8 +1,13 @@
+//Código feito por Lucas Ramos, Bruno Henrique, Eduardo Maximiano, Matheus Landim
+//O código foi feito para calcular a Qualidade da Água 
+//Fonntes e referencias
 #include <stdio.h>
 #include <math.h>
 
 int main (void){
-	double q1, q2, q3, q4, q5, q6, q7, q8, q9, IQA, o2, CF, DBO, DT, FT, NT, pH, ST, TU;
+	//Variaveis para calcular
+	double q1, q2, q3, q4, q5, q6, q7, q8, q9, IQA, o2, CF, DBO, T, FT, NT, pH, RT, TU;
+	//Equação para calcular o oxigenio disolvido
 	printf("O2:");
 	scanf("%lf", &o2);
 			if (o2 == 140){
@@ -10,6 +15,7 @@ int main (void){
 	}	else {
 	q9 = 100.8 * exp(pow(o2 + (-106), 2) / -3745);
 	}
+	//Equação para calcular coliformes fecais 
 	printf("CF:");
 	scanf("%lf", &CF);
 			if (CF > 100000){
@@ -17,6 +23,7 @@ int main (void){
 	} 	else {
 		q1 = 98.03 + (-36.45 * log(CF)) + (3.138 * pow(log10(CF), 2)) + (0.06776 * pow(log10(CF), 3));
 	}
+	//Equação para calcular o PH
 	printf("pH:");
 	scanf("%lf", &pH);
 			if (pH > 12){
@@ -26,6 +33,7 @@ int main (void){
 	}	else {
 		q2 = 0.05421 * pow(pH, (1.23 * pH) + (-0.09873 * pow(pH, 2))) + 5.213;
 	}
+	//Equação para calcular o DBO
 	printf("DBO:");
 	scanf("%lf", &DBO);
 			if (DBO > 30){
@@ -33,6 +41,7 @@ int main (void){
 	}	else {
 		q3 = 102.6 * exp(-0.01101*DBO);
 	}
+	//Equação para calcular o nitrogenio total
 	printf("NT:");
 	scanf("%lf", &NT);
 			if (NT > 100){
@@ -40,6 +49,7 @@ int main (void){
 	} 	else {
 		q4 = 98.96 * pow(NT, (-0.2232) + (-0.006457) * NT);
 	}
+	//Equação para calcular o fosforo total 
 	printf("FT:");
 	scanf("%lf", &FT); 
 			if (FT > 10){
@@ -47,13 +57,15 @@ int main (void){
 	}	else {
 		q5 = 213.7 * exp(-1.680 * pow(FT, 0.3325));
 	}
-	printf("DT:");
-	scanf("%lf", &DT) ;
-			if (DT > 15){
+	//Equação para calcular a temperatura 
+	printf("T:");
+	scanf("%lf", &T) ;
+			if (T > 15){
 				q6 = 9.0;
 	} 	else {
-		q6 = 1 / (0.0003869 * pow(DT + 0.1815, 2) + 0.01081);
+		q6 = 1 / (0.0003869 * pow(T + 0.1815, 2) + 0.01081);
 	} 
+	//Equação para calcular a turbidez
 	printf("TU:");
 	scanf("%lf", &TU);
 			if (TU > 100){
@@ -61,14 +73,15 @@ int main (void){
 	}	else {
 		q7 = 97.34 * exp(-0.01139 * TU) + (-0.04917 * sqrt(TU));
 	}
-	printf("ST:");
-	scanf("%lf", &ST);
-			if (ST > 500){
+	//Equação para calcular o residuo total 
+	printf("RT:");
+	scanf("%lf", &RT);
+			if (RT > 500){
 				q8 = 32.0;
 	}	else {
-		q8 = 80.26 * exp(-0.00107 * ST) + (0.03009 * sqrt(ST) + (-0.1185 * ST));
+		q8 = 80.26 * exp(-0.00107 * RT) + (0.03009 * sqrt(RT) + (-0.1185 * RT));
 	}
-
+	//Calculo do IQA para saber a qualidade da água
 	IQA = pow(q9, 0.17) * pow(q1, 0.15) * pow(q2, 0.12) * pow(q3, 0.10) * pow(q4, 0.10) * pow(q5, 0.10) * pow(q6, 0.1) * pow(q7, 0.08) * pow(q8, 0.08);
 	if (IQA > 79 && IQA <= 100){
 		printf("%lf  = Qualidade Otima!\n", IQA);
